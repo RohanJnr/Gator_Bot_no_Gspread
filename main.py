19b887c3f381af183fc3b0799b5e5054ac34218f
Rohan <icemam449@gmail.com>
2019-01-18 09:27:36 +0530

    tessting on and off for background task

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def background_task():
             else:
                 pass
 
-        if date.today().weekday() == 5:
+        elif date.today().weekday() == 5:
             current_hour = datetime.datetime.now().hour
             current_minute = datetime.datetime.now().minute
             channel = bot.get_channel(254664901102927873)
@@ -49,8 +49,9 @@ async def background_task():
                 await channel.send(' ```Meeting Reminder```***@everyone meeting in 24 Hours ! *** ')
             else:
                 pass
-        """
-        elif date.today().weekday() == 1 or date.today().weekday() == 4:
+
+
+        """elif date.today().weekday() == 1 or date.today().weekday() == 4:
             current_hour = datetime.datetime.now().hour
             current_minute = datetime.datetime.now().minute
             channel = bot.get_channel(480399532253773834)
@@ -70,11 +71,54 @@ async def background_task():
         await asyncio.sleep(60)  # task runs every 60 seconds
 
 
+async def background_task_2():
+
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+
+        if date.today().weekday() == 1 or date.today().weekday() == 4:
+            current_hour = datetime.datetime.now().hour
+            current_minute = datetime.datetime.now().minute
+            channel = bot.get_channel(480399532253773834)
+            if int(current_hour) == 4 and int(current_minute) == 00:
+                await channel.send(' this is a test! ')
+            else:
+                pass
+
+        elif date.today().weekday() == 0 or date.today().weekday() == 3:
+            current_hour = datetime.datetime.now().hour
+            current_minute = datetime.datetime.now().minute
+            channel = bot.get_channel(480399532253773834)
+            if int(current_hour) == 2 and int(current_minute) == 00:
+                await channel.send(' ```War Reminder```*** @everyone shields up ! war spin in 24 hours ! *** ')
+            else:
+                pass
+
+
+await
+asyncio.sleep(60)  # task runs every 60 seconds
+
+
+
 @bot.event
 async def on_ready():
     logging.info('Running as {}'.format(bot.user.name))
     logging.info(bot.user.id)
     await bot.change_presence(activity=discord.Game(name='Where\'s my Water?'))
+
+bot.reminder_task = asyncio.create_task(background_task_2())
+
+
+@commands.command()
+async def cancelwr(ctx):
+    bot.reminder_task.cancel()
+    await ctx.send("war reminders are now turned off")
+
+
+@commands.command()
+async def runwr(ctx):
+    bot.reminder_task = asyncio.create_task(background_task_2())
+    await ctx.send("war reminders are now turned on")
 
 
 bot.loop.create_task(background_task())
