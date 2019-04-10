adb74ae7ba8dda59a3f4f0ef009bc22f73ed55b1
Rohan <icemam449@gmail.com>
2019-04-10 11:55:11 +0530

    argument for number of days check

--- bot/cogs/discord_agecheck.py
+++ bot/cogs/discord_agecheck.py
@@ -18,20 +18,20 @@ class DiscordAgeCheck(commands.Cog):
         await ctx.invoke(self.bot.get_command("help"), "check")
 
     @days_check.command(name='members')
-    async def member_days_check(self, ctx):
+    async def member_days_check(self, ctx, no_of_days=90):
         """shows members who are more than 90 days old in the server."""
         guild = self.bot.get_guild(id=Client.guild)
         # channel = discord.utils.get(guild.channels, id=Channels.bot_logs)
         member_role = discord.utils.get(guild.roles, id=Roles.member)
         embed = discord.Embed()
         embed.colour = 0x01d277
-        embed.title = 'Members who have stayed in the server for more than 90 days !'
+        embed.title = f'Members who have stayed in the server for more than {no_of_days} days !'
         message = ''
         for member in member_role.members:
             today = datetime.datetime.now()
             days_stayed = (today-member.joined_at).days
 
-            if days_stayed > 90:
+            if days_stayed > no_of_days:
 
                 message += '{:<{}s}'.format(member.name, 30) + ' - ' + str(days_stayed) + ' days. \n'
             else:
@@ -40,20 +40,20 @@ class DiscordAgeCheck(commands.Cog):
         await ctx.send(embed=embed)
 
     @days_check.command(name='trials')
-    async def trials_days_check(self, ctx):
+    async def trials_days_check(self, ctx, number_of_days=14):
         """shows trials who are more than 14 days old in the server."""
         guild = self.bot.get_guild(id=Client.guild)
         # channel = discord.utils.get(guild.channels, id=Channels.bot_logs)
         member_role = discord.utils.get(guild.roles, id=Roles.trial)
         embed = discord.Embed()
         embed.colour = 0x01d277
-        embed.title = 'Trials who have stayed in the server for more than 14 days !'
+        embed.title = f'Trials who have stayed in the server for more than {number_of_days} days !'
         message = ''
         for member in member_role.members:
             today = datetime.datetime.now()
             days_stayed = (today - member.joined_at).days
 
-            if days_stayed > 14:
+            if days_stayed > number_of_days:
 
                 message += '{:<{}s}'.format(member.name, 30) + ' - ' + str(days_stayed) + ' days. \n'
             else:
